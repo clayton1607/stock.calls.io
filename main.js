8e1360b1121adc79d4ff8343efbd71339187ebc5
Clayton <claytonpereira1998@gmail.com>
2021-08-20 23:25:01 +0530

    New Option- Premium Service

--- main.js
+++ main.js
@@ -44,13 +44,35 @@ function displayMessage() {
         document.forms['formStock']['displayArea'].value = "Hi Proton Members,"
         +"\n\nStay glued in! Tomorrow morning at 9:30 AM a new stock idea will be announced."
         +"\n\n*Proton Equity*\n*Positive learning Positive earning*";
+    }else if(typeValue == 'premiumService'){
+        document.forms['formStock']['displayArea'].value = "Sign up for our Premium Membership and build the foundation for your financial freedom."
+        +"Proton Equity's premium membership is catered to both the novice Investor looking to build a sound investment portfolio"
+        +" as well as the more experienced investor with a focus on returns. Protons Investing principles are grounded through risk"
+        +" diversification and are driven through research. "
+        +"\n\n*Proton Equity Premium*" 
+        +"\n\n*Invest Lite - 8 Stock Ideas (0 or 1 Monthly)*" 
+        +"\nBuy, Sell & Stop Loss Notices"
+        +"\nHolding Period - 8 - 12 Months"
+        +"\nFees - INR 10,000.00 "
+        +"\nRecommended Investment: INR 15,000 - 20,000 Per Share"
+        +"\n\n*Invest Pro - 20 Stock Ideas (1 or 2 Monthly)*"
+        +"\nBuy, Sell & Stop Loss Notices"
+        +"\nHolding Period - 8 - 12 Months"
+        +"\nCompany Indicator Reports"
+        +"\nFees - INR 20,000.00 "
+        +"\nRecommended Investment - INR 20,000 - 25,000 Per Share"
+        +"\n\n*Proton Equity*"
+        +"\n*Positive learning Positive earning*"
+        +"\n\n*Please Note:*"
+        +"\n_Proton Equity doesn’t engage in speculative investments such as Options, Futures, Cash Derivatives, Crypto etc. "
+        +"Proton Equity is not a SEBI registered Advisor all ideas provided are only insights._";
     }
 }
 
 function changeDisplay(type) {
     console.log(type);
     clearVisibility();
-    if(type == 'stockHype'){
+    if(type == 'stockHype' || type == 'premiumService'){
         document.getElementById('stockName').style.visibility = 'hidden';
     }else{
         document.getElementById('stockName').style.visibility = 'visible';
